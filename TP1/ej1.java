64be0afbaeb961d6163422fedb123b0e86dd63ec
Fran <franciscochang@protonmail.com>
2020-03-23 21:49:47 -0300

    TP1 primera entrega
    probando subir codigo a github para llevar a clase

---
+++ TP1/ej1.java
@@ -0,0 +1,16 @@
+package TP1;
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 0;
+
+		System.out.println("El valor de num1 es" + num1 );
+		
+		num1 = num1 +2;
+		
+		System.out.println("El valor de num1 es" + (num1 * num1) );
+	}
+
+}
---
+++ TP1/ej6.java
@@ -0,0 +1,14 @@
+package TP1;
+import java.util.Scanner;
+public class ej6 {
+	private static Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1
+		System.out.println("Ingres un numero mayor a 0");
+		num1=Integer.parseInt(input.nextline())
+		System.out.println(num1 % 2);	
+	}
+
+}
